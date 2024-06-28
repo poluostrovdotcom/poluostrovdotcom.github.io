13b70a552ff9b714359489a6caeda4631de7ef60
aejiou <khandro.an@gmail.com>
2024-06-28 22:53:18 +0300

    publish story page
    Links to story page added to menu

--- js/menu.js
+++ js/menu.js
@@ -4,7 +4,7 @@ function createMenu(where)
 {
     menu = Array( 
         { url: '/award/index.html', title: 'Премия Имени Леонида Духовного',class:''},
-        { url: 'index.html', title: 'История Клуба',class:''},
+        { url: 'story.html', title: 'История Клуба',class:''},
         { url: 'personalii.html', title: 'Персоналии',class:''},
         // { url: 'investors.html', title: 'Investors',class:''},
         // { url: 'contact.html', title: 'Contact',class:''},
