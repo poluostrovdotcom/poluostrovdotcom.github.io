8ee0a6aef21ac044328d4aa6e4881896a15b92a5
poluostrovdotcom <112909297+poluostrovdotcom@users.noreply.github.com>
2025-05-15 20:50:59 -0700

    Updated rate url for updateSheets

--- js/script.js
+++ js/script.js
@@ -1025,7 +1025,7 @@ function collectData()
 
 function updateSheets(formdata, sheet) {
 	if (sheet == "nomination") var g_url = "https://script.google.com/macros/s/AKfycbxVv3Xofr3fW4kVYHZJ8yibvy7A8x_seiiy4FikZCiuwFk-rHQqcn4GZPhRmBC59eKY/exec";
-	if (sheet == "rate") var g_url = "https://script.google.com/macros/s/AKfycbwjTGIC5rE8pZZnCn-0OemtQWHNRSfz2TIT5BjaWx6VKhjXzUu09lpWzbF_ujVi5zjA/exec"
+	if (sheet == "rate") var g_url = "https://script.google.com/macros/s/AKfycbwxHEId9pLCexp37p4k7Zspva7gmySMERBb5GTeYzoA4nf6uWcgXecwX5cKMWdAMnyW/exec"
 	if (sheet == 'like') var g_url = "https://script.google.com/macros/s/AKfycbxYjxAxPKnV7GkhZLyE2xMQwT91YadVUUah1DPuqvOsZZ3A_rIwqREbpx9ojed48WwhbQ/exec";
 	
 	fetch(g_url, {
